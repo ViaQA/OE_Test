f8ef5eef579258f686c2c52b827af24fbb8e2fa7
Andriy Buryanovaty <andrew.buryanovaty@viaduct.pro>
2022-08-03 19:06:17 +0300

    create spec

---
+++ Pattern.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+npx cypress run --spec "cypress/e2e/OE/01SignInForm.cy.js" --browser chrome --record --key f2351ad0-639d-413a-890a-73a36ee4d09c --tag "Sign In"
+npx cypress run --spec "cypress/e2e/OE/02ListSpaceHost.cy.js" --browser chrome --record --key f2351ad0-639d-413a-890a-73a36ee4d09c --tag "List Space"
+npx cypress run --spec "cypress/e2e/OE/03Admin_approve.cy.js" --browser chrome --record --key f2351ad0-639d-413a-890a-73a36ee4d09c --tag "Approve space"
+npx cypress run --spec "cypress/e2e/OE/04CreateBookingRequest.cy.js" --browser chrome --record --key f2351ad0-639d-413a-890a-73a36ee4d09c --tag "Create booking request"
+npx cypress run --spec "cypress/e2e/OE/05ApproveDeclineBooking.cy.js" --browser chrome --record --key f2351ad0-639d-413a-890a-73a36ee4d09c --tag "Approve than decline request"
+npx cypress run --spec "cypress/e2e/OE/04CreateBookingRequest.cy.js" --browser chrome --record --key f2351ad0-639d-413a-890a-73a36ee4d09c --tag "Create booking request"
+npx cypress run --spec "cypress/e2e/OE/06ApproveRequest.cy.js" --browser chrome --record --key f2351ad0-639d-413a-890a-73a36ee4d09c --tag "Approve request"
+npx cypress run --spec "cypress/e2e/OE/07TransactionSeccesed.cy.js" --browser chrome --record --key f2351ad0-639d-413a-890a-73a36ee4d09c --tag "Send money to host script"
+npx cypress run --spec "cypress/e2e/OE/08Declinerequest.cy.js" --browser chrome --record --key f2351ad0-639d-413a-890a-73a36ee4d09c --tag "Decline request at seccesed payment"
+
+
+
+#// npx cypress run --spec "cypress/e2e/OE/01SignInForm.cy.js" --browser chrome
+#// npx cypress run --spec "cypress/e2e/OE/01SignInForm.cy.js" --browser chrome
