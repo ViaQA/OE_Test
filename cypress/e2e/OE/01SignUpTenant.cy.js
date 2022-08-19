aab0bd148aa07c4a24c259ce73f438283684b117
Andriy Buryanovaty <andrew.buryanovaty@viaduct.pro>
2022-08-19 14:50:48 +0300

    Refactoring Sign Up test to corespond new flow and design

--- cypress/e2e/OE/01SignUpTenant.cy.js
+++ cypress/e2e/OE/01SignUpTenant.cy.js
@@ -1,6 +1,10 @@
 
+import { err_css } from './css';
 import {creds_tenant} from './Var';
 import {creds_host} from './Var';
+
+
+
 
 describe('Check Sign up form', () => {
 
@@ -8,17 +12,21 @@ describe('Check Sign up form', () => {
 
         cy.visit(creds_host.host_url)
         cy.contains('Sign up').click()
-        cy.get('[placeholder="First name"]').click()
-        cy.get('[placeholder="Last name"]').click()
+        cy.get('[data_atr="signUpFirstName"]').click()
+        cy.get('[data_atr="signUpLastName"]').click()
         cy.get('[data_atr="signUpEmail"]').click()
-        cy.get('[placeholder="Password"]').click()
-        cy.contains('Next').click()
-        cy.xpath('//input[@placeholder="First name"]/following-sibling::div').should('have.text', 'Please enter a valid first name')
-        cy.xpath('//input[@placeholder="Last name"]/following-sibling::div').should('have.text', 'Please enter a valid last name')
-        cy.xpath('//input[@data_atr="signUpEmail"]/following-sibling::div').should('have.text', 'Email is a required field')
-        cy.xpath('//input[@placeholder="Password"]/following-sibling::div').should('have.text', 'Password is a required field')
+        cy.get('[data_atr="signUpPassword"]').click()
+        cy.get('[data_atr="signUpPasswordConfirm"]').click()
+        cy.xpath('//input[@data_atr="signUpFirstName"]/following-sibling::div').should('have.text', err_css.err_valid + 'first name').should('have.css', 'color', err_css.err_color)
+        cy.xpath('//input[@data_atr="signUpLastName"]/following-sibling::div').should('have.text',  err_css.err_valid + 'last name').should('have.css', 'color', err_css.err_color)
+        cy.xpath('//input[@data_atr="signUpEmail"]/following-sibling::div').should('have.text', 'Email'+err_css.err_required).should('have.css', 'color', err_css.err_color)
+        cy.xpath('//input[@data_atr="signUpPassword"]/following-sibling::div').should('have.text', 'Password'+err_css.err_required).should('have.css', 'color', err_css.err_color)
         cy.xpath("//span[text()='I confirm and agree to the']/child::a[text()='privacy policy']").should('have.attr', 'href', '/privacy-policy')
         cy.xpath("//span[text()='I confirm and agree to the']/child::a[text()='terms']").should('have.attr', 'href', '/terms-and-conditions')
+        cy.get('[data_atr="signUpFirstName"]').should('have.css', 'border', err_css.err_border)
+        cy.get('[data_atr="signUpLastName"]').should('have.css', 'border', err_css.err_border)
+        cy.get('[data_atr="signUpEmail"]').should('have.css', 'border', err_css.err_border)
+        cy.get('[data_atr="signUpPassword"]').should('have.css', 'border', err_css.err_border)
 
     })
 
@@ -27,33 +35,53 @@ describe('Check Sign up form', () => {
 
         cy.visit(creds_host.host_url)
         cy.contains('Sign up').click()
-        cy.get('[placeholder="First name"]').type("q")
-        cy.get('[placeholder="Last name"]').type("q")
+        cy.get('[data_atr="signUpFirstName"]').type("q")
+        cy.get('[data_atr="signUpLastName"]').type("q")
         cy.get('[data_atr="signUpEmail"]').type("q")
-        cy.get('[placeholder="Password"]').type("q")
+        cy.get('[data_atr="signUpPassword"]').type("q")
+        cy.get('[data_atr="signUpPasswordConfirm"]').click()
+        cy.get('[data_atr="signUpPassword"]').click()
+
+        cy.xpath('//input[@data_atr="signUpPasswordConfirm"]/following-sibling::div').should('have.text', 'Confirm password'+err_css.err_required)
+        cy.get('[data_atr="signUpPasswordConfirm"]').should('have.css', 'border', err_css.err_border)
+    
+        cy.get('[data_atr="signUpPasswordConfirm"]').type("w")
         cy.contains('Next').click()
-        cy.xpath('//input[@placeholder="First name"]/following-sibling::div').should('have.text', 'First name must be at least 2 chracters')
-        cy.xpath('//input[@placeholder="Last name"]/following-sibling::div').should('have.text', 'Last name must be at least 2 characters')
-        cy.xpath('//input[@data_atr="signUpEmail"]/following-sibling::div').should('have.text', 'Email must be a valid email')
-        cy.xpath('//input[@placeholder="Password"]/following-sibling::div').should('have.text', 'Password should be a minimum of 8 characters with at least one uppercase, one number, and one special character')
+        
+        // Have TYPO at error
+        cy.xpath('//input[@data_atr="signUpFirstName"]/following-sibling::div').should('have.text', 'First name'+err_css.err_least)
+        cy.xpath('//input[@data_atr="signUpLastName"]/following-sibling::div').should('have.text', 'Last name'+err_css.err_least)
+        cy.xpath('//input[@data_atr="signUpEmail"]/following-sibling::div').should('have.text', err_css.err_invemail)
+        cy.xpath('//input[@data_atr="signUpPassword"]/following-sibling::div').should('have.text', err_css.err_pass_validation)
+        cy.xpath('//input[@data_atr="signUpPasswordConfirm"]/following-sibling::div').should('have.text', err_css.err_match)
+        cy.get('[data_atr="signUpFirstName"]').should('have.css', 'border', err_css.err_border)
+        cy.get('[data_atr="signUpLastName"]').should('have.css', 'border', err_css.err_border)
+        cy.get('[data_atr="signUpEmail"]').should('have.css', 'border', err_css.err_border)
+        cy.get('[data_atr="signUpPassword"]').should('have.css', 'border', err_css.err_border)
     })
 
-    it("Test type valid data", ()=>{
+    it("Test type valid data and create account", ()=>{
         cy.visit(creds_host.host_url)
         cy.contains('Sign up').click()
-        cy.get('[placeholder="First name"]').type(creds_tenant.firstName)
-        cy.get('[placeholder="Last name"]').type(creds_tenant.lastName)
+        cy.get('[data_atr="signUpFirstName"]').type(creds_tenant.firstName)
+        cy.get('[data_atr="signUpLastName"]').type(creds_tenant.lastName)
         cy.get('[data_atr="signUpEmail"]').type(creds_tenant.email_tenant)
-        cy.get('[placeholder="Password"]').type(creds_tenant.password_tenant)
+        cy.get('[data_atr="signUpPassword"]').type(creds_tenant.password_tenant)
+        cy.get('[data_atr="signUpPasswordConfirm"]').type(creds_tenant.password_tenant)
+
         cy.get('[name="policy"]').click({force: true})
         cy.contains('Next').click()
-        cy.get('[name="phoneNumber"]').click()
+        cy.get('[data_atr="singUpPhoneNumber"]').click()
         cy.contains('Next').click()
-        cy.xpath('//input[@name="phoneNumber"]/following-sibling::div').should('have.text', 'Phone number is a required field')
-        cy.get('[name="phoneNumber"]').type(2)
-        cy.xpath('//input[@name="phoneNumber"]/following-sibling::div').should('have.text', 'Phone number must be at least 11 characters')
-        cy.get('[name="phoneNumber"]').type(creds_tenant.phoneTenant)
+        cy.xpath('//input[@data_atr="singUpPhoneNumber"]/following-sibling::div').should('have.text', 'Phone number'+ err_css.err_required).should('have.css', 'color', err_css.err_color)
+        cy.get('[data_atr="singUpPhoneNumber"]').should('have.css', 'border', err_css.err_border)
+        cy.get('[data_atr="singUpPhoneNumber"]').type(2)
+        cy.xpath('//input[@data_atr="singUpPhoneNumber"]/following-sibling::div').should('have.text', err_css.err_phone_least).should('have.css', 'color', err_css.err_color)
+        cy.get('[data_atr="singUpPhoneNumber"]').should('have.css', 'border', err_css.err_border)
+        cy.get('[data_atr="singUpPhoneNumber"]').type(creds_tenant.phoneTenant)
         cy.contains('Next').click()
+
+        // After update need refactoring
         cy.wait(1000)
         cy.get('[data_atr="codeInput"]').type('111111')
         cy.get('[type="submit"]').click()
---
+++ cypress/e2e/OE/css.js
@@ -0,0 +1,16 @@
+
+// CSS for Sign up form 
+let err_color = 'rgb(209, 41, 27)'
+let err_border = '1.5px solid rgb(209, 41, 27)'
+let err_invemail = 'Invalid email address'
+let err_pass_validation = 'Minimum of 8 characters with at least one uppercase, one number, and one special character'
+let err_pass_confirm = 'Password confirmation does not match'
+let err_valid = 'Please enter a valid '
+let err_least = ' must be at least 2 characters'
+let err_required = ' is a required field'
+let err_match = 'Password confirmation does not match'
+let err_phone_least = 'Phone number must be at least 11 characters'
+
+
+export const err_css = {err_color, err_invemail, err_pass_confirm, err_pass_validation, err_valid, err_least,
+     err_required, err_border, err_match, err_phone_least}
