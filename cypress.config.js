4ac553ab9c3390d94b2164dadd42e7a6fff3387b
Andriy Buryanovaty <andrew.buryanovaty@viaduct.pro>
2022-09-19 16:37:44 +0300

    Add new pattern for run test

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 
 module.exports = defineConfig({
-  viewportWidth: 375,
-  viewportHeight: 677,
+  viewportWidth: 1920,
+  viewportHeight: 1024,
   projectId: 'd6s6gb',
   e2e: {
     defaultCommandTimeout: 10000,
