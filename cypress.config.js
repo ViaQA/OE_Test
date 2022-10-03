2fa04262952ef7880f7aca3d90f16ee912398043
Andriy Buryanovaty <andrew.buryanovaty@viaduct.pro>
2022-10-04 02:01:43 +0300

    Add mocha reporter

--- cypress.config.js
+++ cypress.config.js
@@ -3,14 +3,20 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   viewportWidth: 1980,
-  viewportHeight: 1024,
+  viewportHeight: 1024, 
   video: false,
+  reporter: 'mochawesome',
+  reporterOptions: {
+    overwrite: false,
+    autoOpen: true,
+    timestamp: true
+  },
   projectId: 'd6s6gb',
   e2e: {
+    experimentalSessionAndOrigin: true,
     defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
-      
-
+    
       // implement node event listeners here
     }
   },
