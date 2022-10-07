e97ac19b90434afd4ae3b6898cff1aa49c82fb43
Andriy Buryanovaty <andrew.buryanovaty@viaduct.pro>
2022-10-07 17:17:26 +0300

    Rename duplicate flow test

--- cypress/e2e/OE/TestExecution/02NewListingFlow/06Duplicate.cy.js
+++ cypress/e2e/OE/TestExecution/02NewListingFlow/06Duplicate.cy.js
@@ -3,7 +3,7 @@ import { creds_host, creds_space } from "../../Var"
 import { listData } from "./ListData"
 
 let kindOfSpace = listData.data_event
-describe('Check create listing with back navigation', () => {
+describe('Check duplicate independence space flow', () => {
     beforeEach(() => {
         cy.visit(creds_host.host_url)
         cy.log(Cypress.config("viewportWidth"))
