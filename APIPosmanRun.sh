54f505c32e683657403e5768b6e4196827f10171
Andrew <andrew@Andews-MacBook-Air.local>
2023-01-27 14:01:08 +0200

    Api postman bash script

---
+++ APIPosmanRun.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+postman login --with-api-key PMAK-63c7ebdb162d610df17d1dff-XXXX
+postman collection run 25307064-d0d1c86c-8f08-4ee9-90c0-630d4e0eacc3 -i 25307064-e48109ff-3798-4fd7-9dee-93550b32c904
+postman collection run 25307064-d0d1c86c-8f08-4ee9-90c0-630d4e0eacc3 -i 25307064-e8ae916c-a6ca-49f1-8ecb-ca06596652bf
+postman collection run 25307064-d0d1c86c-8f08-4ee9-90c0-630d4e0eacc3 -i 25307064-267f9ccd-c1f3-42bf-8eae-5e335a7e1d53
+postman collection run 25307064-d0d1c86c-8f08-4ee9-90c0-630d4e0eacc3 -i 25307064-6a02dc9d-2e9b-4cfe-bb11-2da7e1583149
+postman collection run 25307064-d0d1c86c-8f08-4ee9-90c0-630d4e0eacc3 -i 25307064-68eee521-5264-49d6-a1b8-094784949bb1
+postman collection run 25307064-d0d1c86c-8f08-4ee9-90c0-630d4e0eacc3 -i 25307064-8ac1cfef-dc8c-45ee-87c0-412131fd5ea4
+postman collection run 25307064-d0d1c86c-8f08-4ee9-90c0-630d4e0eacc3 -i 25307064-9fa82532-57fd-4521-b8b0-48c1e3f353c0
+postman collection run 25307064-d0d1c86c-8f08-4ee9-90c0-630d4e0eacc3 -i 25307064-1c211e8f-18be-4a4f-81b8-3bb8d7e479bd
+postman collection run 25307064-d0d1c86c-8f08-4ee9-90c0-630d4e0eacc3 -i 25307064-fd3c7310-d1d5-4d7a-9239-be5873227187
+postman collection run 25307064-d0d1c86c-8f08-4ee9-90c0-630d4e0eacc3 -i 25307064-13fed180-0c38-43f3-a4f1-7326a84a56a3
+postman collection run 25307064-d0d1c86c-8f08-4ee9-90c0-630d4e0eacc3 -i 25307064-d45e27c5-d4fb-44d9-bbe4-c3ad5d402640
+postman collection run 25307064-d0d1c86c-8f08-4ee9-90c0-630d4e0eacc3 -i 25307064-3ae4a600-3a92-4291-b73e-980ca4ba724e
